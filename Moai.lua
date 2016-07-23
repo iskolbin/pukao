240aac70fe90852b7127e12dfd31a1fdede46097
iskolbin <iskolbin@gmail.com>
2016-07-23 16:04:24 +0300

    optimized Moai.lua

--- Moai.lua
+++ Moai.lua
@@ -1,55 +1,52 @@
-local type, unpack = type, table.unpack or unpack
+local type, unpack, pairs = type, table.unpack or unpack, pairs
 
 local Moai = {}
 
-local function translateMOAI( class ) 
-	local common, derived = {}, {}
-	
-	local function translateReserve( name_, reserve )
-		local name = name_:match( 'reserve(%w+)s')
-		local newName = name:sub( 1, 1 ):lower() .. name:sub( 2 ) .. 's'
-		local setter = class.getInterfaceTable()['set' .. name]
-		if setter then
-			derived[newName] = function( self, args )
-				reserve( #args )
-				for i = 1, #args do
-					if type( args[i] ) == 'table' then
-						setter( self, i, unpack( args[i] ))
-					else
-						setter( self, args[i] )
-					end
-				end
-			end
-		else
-			derived[newName] = reserve
-		end
-	end
-
-	local function translateInsert( name_, insert )
-		local name = name_:match( 'insert(%w+)' )
-		local newName = name:sub( 1, 1 ):lower() .. name:sub( 2 ) .. 's'
+local function translateReserve( name_, reserve, derived )
+	local name = name_:match( 'reserve(%w+)s')
+	local newName = name:sub( 1, 1 ):lower() .. name:sub( 2 ) .. 's'
+	local setter = class.getInterfaceTable()['set' .. name]
+	if setter then
 		derived[newName] = function( self, args )
+			reserve( #args )
 			for i = 1, #args do
 				if type( args[i] ) == 'table' then
-					insert( self, i, unpack( args[i] ))
+					setter( self, i, unpack( args[i] ))
 				else
-					insert( self, args[i] )
+					setter( self, args[i] )
 				end
 			end
 		end
+	else
+		derived[newName] = reserve
 	end
+end
 
-	local out = {}
-	
+local function translateInsert( name_, insert, derived )
+	local name = name_:match( 'insert(%w+)' )
+	local newName = name:sub( 1, 1 ):lower() .. name:sub( 2 ) .. 's'
+	derived[newName] = function( self, args )
+		for i = 1, #args do
+			if type( args[i] ) == 'table' then
+				insert( self, i, unpack( args[i] ))
+			else
+				insert( self, args[i] )
+			end
+		end
+	end
+end
+
+local function translateMOAI( class ) 
+	local common, derived, out = {}, {}, {}
 	for name, method in pairs( class.getInterfaceTable() ) do
 		if name:sub( 1, 3 ) == 'set' then
 			common[name:sub( 4, 4 ):lower() .. name:sub( 5 )] = method
 		elseif name:sub( 1, 4 ) == 'init' then
 			common[name:sub( 5, 5 ):lower() .. name:sub( 6 )] = method
 		elseif name:match( 'reserve(%w+)s' ) then
-			translateReserve( name, method )
+			translateReserve( name, method, derived )
 		elseif name:match( 'insert(%w+)' ) then
-			translateInsert( name, method )
+			translateInsert( name, method, derived )
 		  --else
 			--print( 'Method not translated:', name )
 		end
